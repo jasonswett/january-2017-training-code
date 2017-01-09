d50f16697e7b666982f9da477dc290132b234256
Jason Swett <jason@benfranklinlabs.com>
2017-01-08 20:34:29 -0500

    Fix model specs.

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @book = FactoryGirl.create(:book)
+  end
+
+  describe 'name' do
+    it 'should be present' do
+      @book.name = nil
+
+      expect(@book).not_to be_valid
+    end
+
+    context 'not unique' do
+      it 'should not be valid' do
+        other_book = @book.dup
+
+        expect(other_book).not_to be_valid
+      end
+    end
+  end
 end
