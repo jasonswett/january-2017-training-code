aa96f4debd43ad66f48faeee4ca9958104ef38af
Jason Swett <jason@benfranklinlabs.com>
2017-02-01 12:13:40 -0500

    More authentication stuff.

--- client/src/app/new-user-form/new-user-form.component.ts
+++ client/src/app/new-user-form/new-user-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Angular2TokenService } from 'angular2-token';
+import { User } from '../user';
 
 @Component({
   selector: 'app-new-user-form',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-user-form.component.css']
 })
 export class NewUserFormComponent implements OnInit {
+  model = new User('', '', '');
 
-  constructor() { }
+  constructor(private _tokenService: Angular2TokenService) {
+    this._tokenService.init({
+      registerAccountPath: '/api/auth',
+      validateTokenPath: '/api/auth/validate_token'
+    });
+  }
 
   ngOnInit() {
   }
 
+  register() {
+    this._tokenService.registerAccount({
+      email:                this.model.email,
+      password:             this.model.password,
+      passwordConfirmation: this.model.password
+    });
+  }
 }
